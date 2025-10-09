3c81b6b2cf3990d1b0cc409ae96de6dee67b3708
chpxu <hi.progracientist@outlook.com>
2025-10-09 17:37:25 +0100

    feat: CONQUEST to .xyz and .extxyz format in Python

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ bohr_to_angstrom = 0.529177249
 @dataclass
 class Atom:
     species: str
-    coords: tuple[float, float, float] | list[float]
-    can_move: tuple[str, str, str] | list[str]
+    coords: list[float]
+    can_move: list[str]
     label: str = ""
 
 
@@ -276,7 +276,76 @@ class vasp_writer(FileWriter):
                     file.write("\n")
 
 
+class xyz_writer(FileWriter):
+    """Class to generate .xyz for basic XYZ file format."""
+
+    def __init__(
+        self,
+        dest: Path | str,
+        data: CONQUEST_COORDINATES,
+        encoding: str = "utf-8",
+        comment_line: str = "comment line",
+    ) -> None:
+        super().__init__(dest, encoding)
+        self.data = data
+        self.comment_line = comment_line
+        self.file = self.open_file()
+        self.write()
+        self.close_file(file=self.file)
+
+    def create_comment_line(self) -> str:
+        return self.comment_line
+
+    def create_atoms_str(self) -> tuple[str, str]:
+        num_ele = self.data.number_of_elements()
+        ele_string = " ".join(list(num_ele.keys()))
+        num_string = " ".join(str(x) for x in num_ele.values())
+        return ele_string, num_string
+
+    def fractional_to_cartesian(self, vector: list[float]) -> list[float]:
+        return [
+            vector[0] * self.data.lattice_vectors[0][0],
+            vector[1] * self.data.lattice_vectors[1][1],
+            vector[0] * self.data.lattice_vectors[2][2],
+        ]
+
+    def write(self) -> None:
+        """XYZ format expects cells in Cartesian coordinates. Since CONQUEST only deals with orthorhombic unit cells, we can just multiply the non-zero coordinates of the lattice vectors with the fractional coordinates"""
+        # ele_string, num_string = self.create_atoms_str()
+        with self.file as file:
+            file.write(f"{self.data.natoms}")
+            file.write(f"{self.create_comment_line()}\n")
+            for atoms in self.data.element_map:
+                for atom in self.data.element_map[atoms]:
+                    file.write(
+                        rf"{atoms} {" ".join(str(x * bohr_to_angstrom) for x in self.fractional_to_cartesian(atom.coords))}"
+                    )
+                    file.write("\n")
+
+
+class extxyz_writer(xyz_writer):
+    def __init__(
+        self,
+        dest: Path | str,
+        data: CONQUEST_COORDINATES,
+        encoding: str = "utf-8",
+        time: float = 0.0,
+    ) -> None:
+        self.time = time
+        super().__init__(dest, data, encoding)
+
+    def create_comment_line(self) -> str:
+        lattice: list[float] = []
+        for single_vector in self.data.lattice_vectors:
+            lattice.append(single_vector[0])
+            lattice.append(single_vector[1])
+            lattice.append(single_vector[2])
+        return f'Lattice="{" ".join(str(x) for x in lattice)}" Properties=species:S:1:pos:R:3 Time={str(self.time)}'
+
+
 conq = CONQUEST_COORDINATES(
     "./test/test.dat", CONQUEST_input=CONQUEST_INPUT({"1": "Bi", "2": "Mn", "3": "O"})
 )
 vasp_writer("test/test.vasp", data=conq)
+xyz_writer("test/test.xyz", data=conq)
+extxyz_writer("test/test.extxyz", data=conq)

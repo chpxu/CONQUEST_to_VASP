44c5628cea300ba3b8f900b2b42dee61cb8f529c
chpxu <dev.chpxu@outlook.com>
2025-12-16 13:12:45 +0000

    feat: manually process non-ASE static output file in read/quantities.py

--- conquest2a/_types.py
+++ conquest2a/_types.py
@@ -1,6 +1,7 @@
 import typing
 import numpy as np
 import numpy.typing as npt
+from pathlib import Path
 
 INTEGER = int | np.int64
 FLOAT = float | np.float64
@@ -8,3 +9,5 @@ REAL_NUMBER = INTEGER | FLOAT
 NUMBER_ARRAY = npt.NDArray[np.number]
 REAL_ARRAY = npt.NDArray[np.float64 | np.int64]
 INT_ARRAY = npt.NDArray[np.int64]
+
+FILE_PATH = str | Path
